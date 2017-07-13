02c98dfc05fdd50906bbdf888f4f23cf2775d1ce
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-07-13 16:04:06 -0400

    Angel says I've got it

--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,40 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
+function handValue(hand) {
+  let numCards = hand.length;
+  let sum = 0;
+  var cardValue;
+  var numAces = 0;
 
-function handValue (hand) {
+  for (var i = 0; i < numCards; i++) {
+    if (hand[i] === "A") {
+      cardValue = 11;
+      numAces++;
+    } else if (hand[i] === "K" || hand[i] === "Q" || hand[i] === "J") {
+      cardValue = 10;
+    } else {
+      cardValue = Number(hand[i]);
+    } // end of if-else
+    sum += cardValue;
+  } // end of for loop
 
-
-  return;
-}
-
+  if (sum > 21 && numAces > 0) {
+    sum = sum - 10 * numAces;
+  }
+  /*  if (sum > 21) {
+    for (var j = 0; j < numCards; j++) {
+      if (hand[i] === "A") {
+        sum -= 10;
+      }
+    }
+  }*/
+  return sum;
+} // end of the function
 
 /* -----  Hints ------
 
